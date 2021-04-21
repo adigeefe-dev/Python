9102d14c39325c0715ea4b8e9b1a17e59e83606f
adigeefe-dev <adigeefe0@gmail.com>
2021-04-21 10:56:09 +0300

    Last touches p8

--- Projects/P8 - Caesar Cipher/test_coverage.py
+++ Projects/P8 - Caesar Cipher/test_coverage.py
@@ -1,18 +1,10 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] #Double for (26+shift_amount)
-should_continue=True
-while should_continue:
+
+while True:
+
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:")
-    if (direction != "encode") and (direction != "decode"):
-        print("Invalid choose")
-        exit()
-
-    text = input("Type your message:").lower()
-
-    try:
-        shift = int(input("Type the shift number:"))
-    except:
-        print("Give an integer")
-        exit()
+    text = input("Type your message:").lower() if (direction == "encode") or (direction == "decode") else exit("Invalid Choose")
+    shift = int(input("Type the shift number:")) if text else exit("Empty input!")
 
     def cipher(start_text,shift_amount,cipher_direction):
         end_text=""
@@ -27,12 +19,11 @@ while should_continue:
                 end_text += alphabet[new_position]
             else:
                 end_text += letter
+        
         print(f"\nYour {cipher_direction}d text:{end_text}")
 
     shift = shift % 26
     cipher(start_text=text,shift_amount=shift,cipher_direction=direction)
-    contuine=input("Do you want to contuine?")
-    if contuine == "no":
-        should_continue=False
-    print("Veni, vidi, utor cipher...")
-    print("Good Bye!")
+    
+    shall_continue=input("Do you want to continue? [yes|no]:")
+    exit("Good Bye!\nVeni, vidi, utor cipher...") if  shall_continue == "no" else 1
